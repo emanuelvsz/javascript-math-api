9a0e54a0a9f35daedb6967434162df4937d4434d
emanuelvsz <usermane13@gmail.com>
2022-06-22 14:43:05 +0000

    refazendo server

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ const express = require('express');
 const app = express();
 
 const PORT = 8000
-app.get('/multiplicacao/:numeroUm/:numeroDois', (requir, rest) => {
-    const numeroUm = +requir.params.numeroUm
-    const numeroDois = +requir.params.numeroDois
-    if(isNaN(numeroUm) || isNaN(numeroDois)){rest.status(400).send('Desculpe, não podemos concluir a operação, envie um número no pedido')}
-    rest.status(200).json({ resultado: numeroUm*numeroDois })
-})
+
 
 app.listen(PORT, () => {
     console.log(`Servidor ouvindo a porta: ${PORT}`);
