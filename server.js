7796ce51e7e26aa2ab9f8f3e6277fee09bf2c3aa
Jefferson Caio <jcoc1@aluno.ifal.edu.br>
2022-07-01 23:09:01 +0000

    branch criada

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 const PORT = 8000
-
+app.get('/subtracao/:numeroUm/:numeroDois', (requir, rest) => {
+    const numeroUm = +requir.params.numeroUm
+    const numeroDois = +requir.params.numeroDois
+    if(isNaN(numeroUm) || isNaN(numeroDois)){rest.status(400).send('Desculpe, não podemos concluir a operação, envie um número no pedido')}
+    rest.status(200).json({ resultado: numeroUm-numeroDois })
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor ouvindo a porta: ${PORT}`);
