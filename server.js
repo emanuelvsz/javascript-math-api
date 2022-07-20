47f3a48f9eb8d602c7ca518ece0296f34d54a198
Renata Beatriz <rbfs1@aluno.ifal.edu.br>
2022-07-20 14:06:06 +0000

    função soma criada

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 
 const PORT = 8000
 
+    app.get('/soma/:numeroUm/:numeroDois', (requir, rest) => {
+        const numeroUm = +requir.params.numeroUm
+        const numeroDois = +requir.params.numeroDois
+        if(isNaN(numeroUm) || isNaN(numeroDois)){rest.status(400).send('Desculpe, não podemos concluir a operação, envie um número no pedido')}
+        rest.status(200).json({ resultado: numeroUm+numeroDois })
+})
 
-app.listen(PORT, () => {
+    app.listen(PORT, () => {
     console.log(`Servidor ouvindo a porta: ${PORT}`);
 })
