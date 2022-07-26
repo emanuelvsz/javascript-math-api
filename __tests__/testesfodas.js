26dc6fff1591c40c841e4a54c651cd7693d32cf0
emanuelvsz <usermane13@gmail.com>
2022-07-26 12:56:28 +0000

    criada a branch de multiplicacao

--- __tests__/testesfodas.js
+++ __tests__/testesfodas.js
@@ -2,7 +2,13 @@ const request = require('supertest')
 const app = require('../routes')
 
 test('teste subtracao', async () => {
-    const response = await request(app).get('/subtracao/2')
+    const response = await request(app).get('/subtracao/2/1')
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({ resultado: 1 })
+})
+
+test('teste multiplicacao', async () => {
+    const response = await request(app).get('/multiplicacao/2/1')
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({ resultado: 2 })
 })
--- routes.js
+++ routes.js
@@ -8,4 +8,11 @@ app.get('/subtracao/:numeroUm/:numeroDois', (requir, rest) => {
     rest.status(200).json({ resultado: numeroUm-numeroDois })
 })
 
+app.get('/multiplicacao/:numeroUm/:numeroDois', (requir, rest) => {
+    const numeroUm = +requir.params.numeroUm
+    const numeroDois = +requir.params.numeroDois
+    if(isNaN(numeroUm) || isNaN(numeroDois)){rest.status(400).send('Desculpe, não podemos concluir a operação, envie um número no pedido')}
+    rest.status(200).json({ resultado: numeroUm*numeroDois })
+})
+
 module.exports = app;
