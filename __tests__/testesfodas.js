65a7c803b9bd34bd672b1293a76665ad7f1d2942
Renata Beatriz <rbfs1@aluno.ifal.edu.br>
2022-07-27 00:53:48 +0000

    Testes de soma criados

--- __tests__/testesfodas.js
+++ __tests__/testesfodas.js
@@ -2,13 +2,13 @@ const request = require('supertest')
 const app = require('../routes')
 
 test('teste subtracao', async () => {
-    const response = await request(app).get('/subtracao/2/1')
+    const response = await request(app).get('/subtracao/2')
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({ resultado: 1 })
 })
 
-test('teste multiplicacao', async () => {
-    const response = await request(app).get('/multiplicacao/2/1')
+test('teste soma', async () => {
+    const response = await request(app).get('/soma/2/1')
     expect(response.statusCode).toBe(200)
-    expect(response.body).toEqual({ resultado: 2 })
+    expect(response.body).toEqual({ resultado: 3 })
 })
