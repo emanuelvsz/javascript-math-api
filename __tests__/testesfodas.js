5c809b7f4c49c58ed76af319675e364c0f5fb7ca
Renata Beatriz <rbfs1@aluno.ifal.edu.br>
2022-07-27 00:55:13 +0000

    Rotas atualizadas com a soma

--- __tests__/testesfodas.js
+++ __tests__/testesfodas.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../routes')
 
 test('teste subtracao', async () => {
-    const response = await request(app).get('/subtracao/2')
+    const response = await request(app).get('/subtracao/2/1')
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({ resultado: 1 })
 })
