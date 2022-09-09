5c09e9a0561f01741c3855839e9a626a23a1e07c
emanuelvsz <usermane13@gmail.com>
2022-09-09 12:41:49 +0000

    corrigindo erro nno teste unitario

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -4,4 +4,7 @@ describe('funções matemáticas da calculadora', () => {
     it('multiplica dois números', () => {
         expect(mult(1,2)).toBe(2)
     })
+    it('deu errado', () => {
+        expect(mult('')).toBe(undefined)
+    })
 })
