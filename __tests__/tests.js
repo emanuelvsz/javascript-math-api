12c38e2a9448943a922c115760f5c6c808c4d747
Emanuel Vilela <84058517+emanuelvsz@users.noreply.github.com>
2022-10-04 18:09:35 -0300

    Update tests.js

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,7 +1,16 @@
-const {somar} = require('../routes')
+const {somar, multiplicar, subtrair, dividir} = require('../routes')
 
 describe('funções matemáticas da calculadora', () => {
     it('soma dois números', () => {
         expect(somar(1,2)).toBe(3)
-    })
+    });
+    it('subtrai dois números', () => {
+        expect(subtrair(10,2)).toBe(3)
+    });
+    it('multiplica dois números', () => {
+        expect(multiplicar(2,5)).toBe(3)
+    });
+    it('divide dois números', () => {
+        expect(dividir(10,5)).toBe(3)
+    });
 })
