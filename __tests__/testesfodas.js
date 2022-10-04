dfed660aa4690e7a3ab72ba619b6d81ca6524d4c
Emanuel Vilela <84058517+emanuelvsz@users.noreply.github.com>
2022-10-04 18:10:01 -0300

    Update testesfodas.js

--- __tests__/testesfodas.js
+++ __tests__/testesfodas.js
@@ -6,7 +6,6 @@ test('teste subtracao', async () => {
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({ resultado: 1 })
 })
-
 test('teste soma', async () => {
     const response = await request(app).get('/soma/2/1')
     expect(response.statusCode).toBe(200)
