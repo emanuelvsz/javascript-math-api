d264dbd098e227d2350735903593678832f3c8d4
evic08 <103127737+evic08@users.noreply.github.com>
2022-10-05 10:48:03 -0300

    Update tests.js

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -5,12 +5,12 @@ describe('funções matemáticas da calculadora', () => {
         expect(somar(1,2)).toBe(3)
     });
     it('subtrai dois números', () => {
-        expect(subtrair(10,2)).toBe(3)
+        expect(subtrair(10,2)).toBe(8)
     });
     it('multiplica dois números', () => {
-        expect(multiplicar(2,5)).toBe(3)
+        expect(multiplicar(2,5)).toBe(10)
     });
     it('divide dois números', () => {
-        expect(dividir(10,5)).toBe(3)
+        expect(dividir(10,5)).toBe(2)
     });
 })
